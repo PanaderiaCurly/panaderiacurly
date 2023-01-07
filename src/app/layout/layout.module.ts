43ad6a9e77359d829183baad8b3bc1e3858cb259
MSGB <msg.barraza@gmail.com>
2023-01-07 03:11:02 -0300

    [Bitácora N°2] Creando start-up de la aplicación web (parte 2) 📄

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoadingBarModule } from '@ngx-loading-bar/core';
+import { LoadingBarRouterModule } from '@ngx-loading-bar/router';
 
-
+import { NgxLoadingBarComponent } from './ngx-loading-bar/ngx-loading-bar.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NgxLoadingBarComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    LoadingBarModule,
+    LoadingBarRouterModule
+  ],
+  exports: [
+    NgxLoadingBarComponent
+  ],
+  providers: []
 })
 export class LayoutModule { }
---
+++ src/app/layout/ngx-loading-bar/ngx-loading-bar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ngx-loading-bar',
+  templateUrl: './ngx-loading-bar.component.html',
+  styles: [
+  ]
+})
+export class NgxLoadingBarComponent { }
